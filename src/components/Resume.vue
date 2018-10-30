f4c3dc3138e12f2ac2456a5572392a80a96fc0fb
endotnick <endotnick@gmail.com>
2018-10-29 19:31:08 -0700

    test pdfvuer

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
      <p>Resume view rendered</p>
+     <pdf src="https://github.com/endotnick/endotnick.github.io/raw/development/src/assets/NickRodriguezResume_public.pdf" :page=1></pdf>
   </div>
 </template>
 
 <script>
+import pdf from 'pdfvuer';
+
 export default {
   name: 'Resume',
+
+  components: {
+    pdf,
+  },
 };
 </script>
 
